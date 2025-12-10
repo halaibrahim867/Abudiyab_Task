7852c607386d9bf31516a167b2b3baf4b6102703
Hala Ibrahim <halaibrahim867@gmail.com>
2025-12-10 14:44:36 +0200

    add base of payment

---
+++ app/Interfaces/PaymentGatewayInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Interfaces;
+
+use Illuminate\Http\Request;
+
+interface PaymentGatewayInterface
+{
+
+    public function sendPayment(Request $request);
+
+    public function callBack(Request $request);
+}
